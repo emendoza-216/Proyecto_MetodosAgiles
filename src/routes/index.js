e15387c7a938203834846770dadf90fcd3464f8a
gochoa032 <58501727+gochoa032@users.noreply.github.com>
2020-12-03 21:59:28 -0800

    Limpieza y mensajes de error mas exactos

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,103 +19,6 @@ router.get('/', async (req, res) => {
     res.render('principal');
 })
 
-/*
-router.post('/asistencias', async (req, res, next) => {
-    var grupo = await conexion.obtenerGrupo(grupo);
-    var dirArchivo = req.body.dirArchivo;
-    var curso = await conexion.obtenerCurso(curso);
-    var archivosRecibidos = 0;
-
-    var fstream;
-    req.pipe(req.busboy);
-
-    req.busboy.on('field', function (fieldname, val) {
-        if (fieldname == 'curso') {
-            curso = val;
-        }
-        if (fieldname == 'archivosRecibidos') {
-            archivosRecibidos = val;
-            console.log("Recibiendo: " + val);
-        }
-    });
-
-    var archivoSubido = [];
-    var listaGrupos = [];
-    function mostrarSiguiente() {
-        conexion.obtenerAsistenciasCallback((listaAsistencia) => {
-            res.render('RegistrarAsistencias', { listaAsistencia, listaGrupos, archivoSubido });
-        });
-    }
-
-    function ejecutar(direccion, filename) {
-        lecturaArchivos.leerArchivo(direccion, async (listaObj) => {
-            //const curso = listaObj.curso;
-            const existe = await conexion.obtenerCurso(curso);
-            if (existe[0] == null) {
-                res.render('cursos', { res: 2, prellenado: curso });
-            }
-            else {
-                if (typeof (grupo) == "undefined") {
-                    console.log("grupo no definido todavia");
-
-                    if (listaGrupos[0] == null) {
-                        const grupos = await grupoModel.find();
-                        for (let index = 0; index < grupos.length; index++) {
-                            const grupo = grupos[index];
-                            if (grupo.curso.equals(existe[0]._id)) {
-                                listaGrupos.push(grupo);
-                            }
-                        }
-                    }
-
-                    if (listaGrupos[0] == null) {
-                        const cursos = await cursoModel.find();
-                        res.render('grupos', { res: 2, cursos });
-                    }
-                    else {
-                        console.log("archivo push");
-                        archivoSubido.push(filename);
-                        //mostrarSiguiente();
-                    }
-                }
-                else {
-                    console.log("si hay grupo");
-                    listaObj.grupo = grupo;
-                    conexion.registrarAsistencia(listaObj, async (err) => {
-                        console.log("Registrada.")
-                    });
-                }
-            }
-        });
-    }
-
-    var up = 0;
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename);
-        var direccion = 'tmp/' + filename;
-        fstream = fs.createWriteStream(direccion);
-        file.pipe(fstream);
-        fstream.on('close', function () {
-            ejecutar(direccion, filename);
-            up++;
-            if (up == archivosRecibidos)
-                mostrarSiguiente();
-        });
-    });
-
-    if (typeof (dirArchivo) != "undefined") {
-        var archivosArr = dirArchivo.split(",");
-
-        for (let index = 0; index < archivosArr.length; index++) {
-            const nom = archivosArr[index];
-            const direccion = 'tmp/' + nom;
-            ejecutar(direccion, nom);
-        }
-
-        //res.redirect('back');
-    }
-});
-*/
 router.post('/asistencias', async (req, res, next) => {
     var grupo = req.body.grupo;
     var curso = req.body.curso;
@@ -154,11 +57,16 @@ router.post('/asistencias', async (req, res, next) => {
     
                 //console.log(listaObj);
                 conexion.registrarAsistencia(listaObj, async (err) => {
-                    console.log("Registrada.");
+                    if(err != null){
+                        res.json({res: "Ha ocurrido un error desconocido al subir los archivos."});
+                    }
+                    else {
+                        console.log("Registrada.");
 
-                    up++;
-                    if (up == archivosRecibidos) { //si se terminaron de subir todos
-                        res.json({res: "Archivos subidos exitosamente."});
+                        up++;
+                        if (up == archivosRecibidos) { //si se terminaron de subir todos
+                            res.json({res: "Archivos subidos exitosamente."});
+                        }
                     }
                 });
             }
@@ -195,16 +103,6 @@ router.post('/asistencias', async (req, res, next) => {
     })
 });
 
-/*
-router.get('/asistencias', async (req, res) => {
-    //mostrar tabla
-    const cursos = await cursoModel.find();
-    conexion.obtenerAsistenciasCallback((listaAsistencia) => {
-        //console.log(listaAsistencia);
-        res.render('principal', { listaAsistencia, cursos, listaGrupos: [], archivoSubido: [] });
-    });
-});
-*/
 router.get('/asistencias/:modo/:filtro', async (req, res) => {
     var modo = req.params.modo;
     var filtro = req.params.filtro;
@@ -240,11 +138,6 @@ router.get('/asistencias/:modo/:filtro', async (req, res) => {
     }
 });
 
-/*
-router.get('/cursos', async (req, res) => {
-    res.render('cursos', { res: null, prellenado: null });
-});
-*/
 router.get('/cursos', async (req, res) => {
     const cursos = await cursoModel.find();
     res.json(cursos);
@@ -254,8 +147,17 @@ router.post('/cursos', async (req, res, next) => {
     const curso = req.body.nombre;
     console.log(req.body);
     const regex = new RegExp('^[a-zA-ZÀ-ÿ _\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ _\u00f1\u00d1]*)+$', 'i');
-    if (typeof (curso) == "undefined" || curso.length > 60 || !regex.test(curso)) { // No es válido.
-        res.json({res: "No es válido.", prellenado: null });
+
+    var err = null;
+    if(typeof (curso) == "undefined")
+        err = "Curso no definido.";
+    if(curso.length > 60)
+        err = "El curso debe ser igual o menor a 60 caracteres.";
+    if(!regex.test(curso))
+        err = "Introduzca solo letras (A-Z) o (a-z). Máximo 60 caracteres. Evite espacios al inicio o final del nombre."
+
+    if (err != null) { // No es válido.
+        res.json({res: err, prellenado: null });
     } else {
         const existe = await conexion.obtenerCurso(curso);
         if (existe == null) {
@@ -282,46 +184,11 @@ router.post('/cursos', async (req, res, next) => {
     }
 });
 
-/*
-router.post('/cursos', async (req, res, next) => {
-    const curso = req.body.curso;
-    const regex = new RegExp('^[a-zA-ZÀ-ÿ _\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ _\u00f1\u00d1]*)+$', 'i');
-    if (typeof (curso) == "undefined" || curso.length > 60 || !regex.test(curso)) { // No es válido.
-        res.render('cursos', { res: null, prellenado: null });
-    } else {
-        const existe = await conexion.obtenerCurso(curso);
-        if (existe[0] == null) {
-            console.log("no esta repetido se crea [0]");
-            conexion.crearCurso(curso);
-            res.render('cursos', { res: 0, prellenado: null });
-        }
-
-        for (let index = 0; index < existe.length; index++) {
-            const element = existe[index];
-            if (curso.toUpperCase() == element.nombre.toUpperCase()) { // Ya existe.
-                console.log("esta repetido")
-                res.status(401).render('cursos', { res: 1, prellenado: null });
-                break;
-            }
-            if (index + 1 == existe.length) { // No existe.
-                console.log("no esta repetido se crea")
-                conexion.crearCurso(curso);
-                res.render('cursos', { res: 0, prellenado: null });
-            }
-        }
-    }
-});
-*/
-/*
-router.get('/grupos', async (req, res) => {
-    const cursos = await cursoModel.find();
-    res.render('grupos', { res: null, cursos });
-});
-*/
 router.get('/grupos', async (req, res) => {
     const grupos = await grupoModel.find();
     res.json(grupos);
 });
+
 router.get('/grupos/:curso', async(req, res) => {
     const curso = req.params.curso;
 
@@ -350,8 +217,18 @@ router.post('/grupos', async (req, res, next) => {
     const curso = req.body.curso;
     const regex = new RegExp('^[a-zA-ZÀ-ÿ _\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ _\u00f1\u00d1]*)+$', 'i');
 
-    if (typeof (grupo) == "undefined" || typeof (curso) == "undefined" || grupo.length > 50 || !regex.test(grupo)) { // No es válido.
-        res.json({res: "No es válido."});
+    var err = null;
+    if(typeof (curso) == "undefined")
+        err = "Curso no definido.";
+    if(typeof (grupo) == "undefined")
+        err = "Grupo no definido.";
+    if(grupo.length > 50)
+        err = "El grupo debe ser igual o menor a 50 caracteres.";
+    if(!regex.test(grupo))
+        err = "Introduzca solo letras (A-Z) o (a-z). Máximo 50 caracteres. Evite espacios al inicio o final del nombre."
+
+    if (err != null) { // No es válido.
+        res.json({res: err});
     } else {
         const existe = await conexion.obtenerGrupo(grupo);
         if (existe == null) {
@@ -378,39 +255,6 @@ router.post('/grupos', async (req, res, next) => {
     }
 });
 
-/*
-router.post('/grupos', async (req, res, next) => {
-    const cursos = await cursoModel.find();
-    const grupo = req.body.grupo;
-    const curso = req.body.curso;
-    const regex = new RegExp('^[a-zA-ZÀ-ÿ _\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ _\u00f1\u00d1]*)+$', 'i');
-
-    if (typeof (grupo) == "undefined" || typeof (curso) == "undefined" || grupo.length > 50 || !regex.test(grupo)) { // No es válido.
-        res.render('grupos', { cursos, res: null });
-    } else {
-        const existe = await conexion.obtenerGrupo(grupo);
-        if (existe[0] == null) {
-            console.log("no esta repetido se crea")
-            await conexion.crearGrupo(grupo, curso);
-            res.render('grupos', { cursos, res: 0 });
-        }
-
-        for (let index = 0; index < existe.length; index++) {
-            const element = existe[index];
-            if (grupo.toUpperCase() == element.nombre.toUpperCase()) { // Ya existe.
-                console.log("esta repetido")
-                res.status(401).render('grupos', { cursos, res: 1 });
-                break;
-            }
-            if (index + 1 == existe.length) { // No existe.
-                console.log("no esta repetido se crea")
-                await conexion.crearGrupo(grupo, curso);
-                res.render('grupos', { cursos, res: 0 });
-            }
-        }
-    }
-});
-*/
 router.get('/unidades', async (req, res) => {
     res.json([]);
 });
